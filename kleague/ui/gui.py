e1ee1ca2add8b929d5f9876e4e2ca33a28256105
bsmmoon <bsmmoon@gmail.com>
2015-12-05 13:09:27 +0800

    a bit of mainframe

--- kleague/ui/gui.py
+++ kleague/ui/gui.py
@@ -16,54 +16,79 @@ class GUI(Frame):
             self.root.rowconfigure(r, weight=1)    
         for c in range(cols):
             self.root.columnconfigure(c, weight=1)
-            # Button(self.root, text="Button {0}".format(c)).grid(row=6,column=c,sticky=E+W)
 
-        for r in range(rows):
-            for c in range(cols):
-                Label(self.root, text='(' + str(r) + ',' + str(c) +')',
-                    borderwidth = 1).grid(row = r, column = c)
-
-        self.MenuFrame = Frame(self.root, bg="red")
-        self.MenuFrame.grid(row = 9, column = 0, rowspan = 1, columnspan = cols, sticky = W+E+N+S)
+        menuFrame = Frame(self.root, bg="red")
+        menuFrame.grid(row = rows-1, column = 0, rowspan = 1, columnspan = cols, sticky = W+E+N+S)
         for c in range(cols):
-            self.MenuFrame.columnconfigure(c, weight=1)
-        self.MenuFrame.rowconfigure(0, weight=1)
+            menuFrame.columnconfigure(c, weight=1)
+        menuFrame.rowconfigure(0, weight=1)
         
-        self.HeaderFrame = Frame(self.root, bg="blue")
-        self.HeaderFrame.grid(row = 0, column = 0, rowspan = 1, columnspan = cols, sticky = W+E+N+S)
+        headerFrame = Frame(self.root, bg="blue")
+        headerFrame.grid(row = 0, column = 0, rowspan = 1, columnspan = cols, sticky = W+E+N+S)
         for c in range(cols):
-            self.HeaderFrame.columnconfigure(c, weight=1)
-        self.HeaderFrame.rowconfigure(0, weight=1)
+            headerFrame.columnconfigure(c, weight=1)
+        headerFrame.rowconfigure(0, weight=1)
 
-        self.createMenu()
+        mainFrame = Frame(self.root)
+        mainFrame.grid(row = 1, column = 0, rowspan = rows - 2, columnspan = cols, sticky = W+E+N+S)
+        for r in range(rows):
+            mainFrame.rowconfigure(r, weight=1)
+        for c in range(cols):
+            mainFrame.columnconfigure(c, weight=1)
+        mainFrame.rowconfigure(0, weight=1)
 
-        self.transfercentreIsOpen = False
+        self.openedMenu = 0
 
-    def createMenu(self):
-        Button(self.MenuFrame, text="Menu1", command=self.say_hi).grid(row=0,column=0,columnspan=2,sticky=E+W)
-        Button(self.MenuFrame, text="Menu2", command=self.transfercentre).grid(row=0,column=2,columnspan=2,sticky=E+W)
-        Button(self.MenuFrame, text="Menu3", command=self.transfercentre).grid(row=0,column=4,columnspan=2,sticky=E+W)
+        self.createMenu(menuFrame, mainFrame)
+        self.updateHeader(headerFrame)
 
-    def transfercentre(self):
-        if self.transfercentreIsOpen:
+    def createMenu(self, menuFrame, mainFrame):
+        Button(menuFrame, text="Menu1", command=self.say_hi).grid(row=0,column=0,columnspan=2,sticky=E+W)
+        Button(menuFrame, text="Transfer Centre", command=self.transfercentre(mainFrame)).grid(row=0,column=2,columnspan=2,sticky=E+W)
+        Button(menuFrame, text="Menu3", command=self.transfercentre(mainFrame)).grid(row=0,column=4,columnspan=2,sticky=E+W)
+
+    def updateHeader(self, headerFrame):
+        if self.openedMenu == 0:
+            Label(headerFrame, text='0', borderwidth = 1).grid(row = 0, column = 0, columnspan = 6)
+        elif self.openedMenu == 1:
+            Label(headerFrame, text='Transfer Centre', borderwidth = 1).grid(row = 0, column = 0, columnspan = 6)
+        elif self.openedMenu == 2:
+            Label(headerFrame, text='2', borderwidth = 1).grid(row = 0, column = 0, columnspan = 6)
+
+    def transfercentre(self, mainFrame):
+        if self.openedMenu == 1:
             return
+        else:
+            self.openedMenu = 1
 
-        self.search_player = Entry(self)
-        self.search_player.pack(side='left')
+        self.labelFrame(mainFrame, 8, 6)
 
-        self.search_team = Entry(self)
-        self.search_team.pack(side='left')
+        Label(mainFrame, text='Player').grid(row=0, column=0, columnspan=3)
+        search_player = Entry(mainFrame)
+        search_player.grid(row=1, column=0, columnspan=3)
 
-        self.root.after(2000, self.repeatTest)
+        Label(mainFrame, text='Team').grid(row=0, column=3, columnspan=3)
+        search_team = Entry(mainFrame)
+        search_team.grid(row=1, column=3, columnspan=3)
+        
+        # self.root.after(2000, self.repeatTest(search_player, search_team))
 
-        self.transfercentreIsOpen = True
 
     def say_hi(self):
         print("hi there, everyone!")
 
-    def repeatTest(self):
-        print(self.search_player.get() + ' to ' + self.search_team.get())
-        self.root.after(2000, self.repeatTest)
+    def repeatTest(self, search_player, search_team):
+        if self.openedMenu != 1:
+            return
+        print(search_player.get() + ' to ' + search_team.get())
+        self.root.after(2000, self.repeatTest(search_player, search_team))
 
     def run(self):
         self.mainloop()
+
+    def labelFrame(self, frame, rows, cols):
+        for r in range(rows):
+            for c in range(cols):
+                Label(frame, text='(' + str(r) + ',' + str(c) +')',
+                    borderwidth = 1).grid(row = r, column = c)
+
